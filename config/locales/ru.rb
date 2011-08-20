71960ef11e6913809487f6fbb32f156b75a7fa14
Dmitri Koulikoff (vaio) <dima@koulikoff.ru>
2011-08-20 19:22:15 +0300

    Russian support with pluralization

---
+++ config/locales/ru.rb
@@ -0,0 +1,21 @@
+{
+  :ru => {
+    :i18n => {
+      :plural => {
+        :rule => lambda { |n|
+          if n % 10 == 1 && n % 100 != 11
+            :one
+          elsif [2, 3, 4].include?(n % 10) &&
+              ![12, 13, 14].include?(n % 100)
+            :few
+          elsif n % 10 == 0 || [5, 6, 7, 8, 9].include?(n % 10) ||
+              [11, 12, 13, 14].include?(n % 100)
+            :many
+          else
+            :other
+          end
+        }
+      }
+    }
+  }
+}
